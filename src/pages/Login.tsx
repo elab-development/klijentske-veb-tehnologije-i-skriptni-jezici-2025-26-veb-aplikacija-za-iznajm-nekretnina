cad164bf5f5f34b2aba627f216be6e9e4dc2171a
Vuk Borovic <vuk.ucz@gmail.com>
2026-07-04 23:31:55 +0200

    Dodate osnovne stranice aplikacije

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Ovo je Login stranica</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Ovo je Register stranica</h1>
+    </div>
+  );
+};
+
+export default Register;

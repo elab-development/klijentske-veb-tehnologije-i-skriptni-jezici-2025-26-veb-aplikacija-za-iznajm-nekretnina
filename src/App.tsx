a380e109703a56edf1b2f9bc181048c4b1119582
Vuk Borovic <vuk.ucz@gmail.com>
2026-07-04 23:37:30 +0200

    Implementiran react-router-dom i navigacija izmedju stranica

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Properties from './pages/Properties';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+        <Link to="/">Početna</Link> |{' '}
+        <Link to="/nekretnine">Nekretnine</Link> |{' '}
+        <Link to="/prijava">Prijava</Link> |{' '}
+        <Link to="/registracija">Registracija</Link>
+      </nav>
+
+      <div style={{ padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nekretnine" element={<Properties />} />
+          <Route path="/prijava" element={<Login />} />
+          <Route path="/registracija" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Properties.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Properties = () => {
+  return (
+    <div>
+      <h1>Ovo je Properties stranica</h1>
+    </div>
+  );
+};
+
+export default Properties;
